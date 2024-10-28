2ab67e49229c0391b1afc236918365b6c26ba568
massinger <10529946+massinger@users.noreply.github.com>
2024-10-28 14:22:14 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.gplus.eu.org": dockerHub,
+  "quay.gplus.eu.org": "https://quay.io",
+  "gcr.gplus.eu.org": "https://gcr.io",
+  "k8s-gcr.gplus.eu.org": "https://k8s.gcr.io",
+  "k8s.gplus.eu.org": "https://registry.k8s.io",
+  "ghcr.gplus.eu.org": "https://ghcr.io",
+  "cloudsmith.gplus.eu.org": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "docker-staging.gplus.eu.org": dockerHub,
 };
 
 function routeByHosts(host) {
